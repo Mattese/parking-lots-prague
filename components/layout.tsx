4f4f728ec08eb7d349d3a2f05f24e8bbaaa1456f
martin.reichl@logsteo.com <martin.reichl@logsteo.com>
2021-03-08 13:43:56 +0100

    Basic layout done

--- components/layout.tsx
+++
@@ -1,8 +0,0 @@
-export const Layout: React.FC = (children) => {
-  return (
-    <>
-      <div></div>
-      <div>{children}</div>
-    </>
-  );
-};
---
+++ components/layout/layout.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import styles from "./layout.module.scss";
+import Nav from "./nav";
+
+export const Layout: React.FC = ({ children }) => {
+  const [sidePanelHidden, setSidePanelHidden] = useState(false);
+  return (
+    <div className={styles.layoutWrapper}>
+      <div
+        className={sidePanelHidden ? styles.sidePanelHidden : styles.sidePanel}
+      >
+        <Nav sidePanelHidden={sidePanelHidden} />
+        <div
+          className={styles.sidePanelControlElement}
+          onClick={() => {
+            setSidePanelHidden((prevState) => !prevState);
+          }}
+        >
+          {sidePanelHidden ? <h3>Open</h3> : <h3> Hide</h3>}
+        </div>
+      </div>
+      <div className={styles.body}>
+        <>
+          {children}
+        </>
+      </div>
+    </div>
+  );
+};
